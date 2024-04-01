a5a54512590db17173c6c664b00884e63231234a
Aqsa Shaikh <160129099+Aqsamajeed00@users.noreply.github.com>
2024-04-01 22:28:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+//Conditional ternary operators (boolean operators)
+Object.defineProperty(exports, "__esModule", { value: true });
+// let hunger = true
+// let snack = hunger? "eat apple" : "drink water"
+// console.log(`you should ${snack}`);
+let hunger = false;
+let snack = hunger ? "eat apple" : "drink water";
+console.log(`you should ${snack}`);
---
+++ main.ts
@@ -0,0 +1,10 @@
+//Conditional ternary operators (boolean operators)
+
+// let hunger = true
+// let snack = hunger? "eat apple" : "drink water"
+// console.log(`you should ${snack}`);
+
+let hunger = false
+let snack = hunger? "eat apple" : "drink water"
+
+console.log(`you should ${snack}`);
